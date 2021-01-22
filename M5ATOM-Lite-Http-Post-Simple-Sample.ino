ea93156b959e4687c81e70ea8c6c082015633855
takahiro mitsuoka <tmitsuoka0423@gmail.com>
2021-01-22 21:40:50 +0900

    add

---
+++ M5ATOM-Lite-Http-Post-Simple-Sample.ino
@@ -0,0 +1,33 @@
+#define FASTLED_INTERNAL
+
+#include <M5Atom.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+void post() {
+  HTTPClient http;
+  http.begin("http://httpbin.org/post");
+  http.addHeader("Content-Type", "application/json");
+  http.POST("{\"title\": \"test\"}");
+  Serial.println(http.getString());
+  http.end();
+}
+
+void setup() {
+  M5.begin();
+  Serial.begin(115200);
+
+  Serial.print("WiFi connecting.");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(100);
+  }
+
+  post();
+}
+
+void loop() { /* 起動時 */}
---
+++ README.md
@@ -0,0 +1,5 @@
+# m5atom-lite-http-post-simple-sample
+
+M5ATOM Lite から HTTP POST するシンプルなサンプルコードです。
+
+[![Image from Gyazo](https://i.gyazo.com/15c56924f5ad9d5a4e91d345f5d0fcec.jpg)](https://gyazo.com/15c56924f5ad9d5a4e91d345f5d0fcec)
